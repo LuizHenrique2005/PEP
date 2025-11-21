a5b09418a2900ed6f3069f0c5926d83b30345e15
RailanTeles <135670809+RailanTeles@users.noreply.github.com>
2025-11-20 23:42:24 -0300

    tela de início feita

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,43 @@ export default function Home() {
       <SideBar />
       <div className="ml-[18%] flex-1">
         <NavBar />
+        <div className="flex w-full pt-30 justify-evenly flex-wrap">
+          <div className="w-[40%] shadow-2xl shadow-black flex flex-col p-10">
+            <div className="grid grid-cols-[20%_80%] grid-rows-2">
+              <div>
+                <img src="/Images/agenda-icone.png" alt="" />
+              </div>
+              <div className="flex items-center text-[#07122D] text-3xl font-medium">
+                <p>Agenda</p>
+              </div>
+              <div></div>
+              <div>
+                <p>Verificar atendimentos agendados</p>
+              </div>
+            </div>
+            <p className="text-[#888585] text-2xl cursor-pointer pt-10">
+              Minha Agenda
+            </p>
+          </div>
+
+          <div className="w-[40%] shadow-2xl shadow-black flex flex-col p-10">
+            <div className="grid grid-cols-[20%_80%] grid-rows-2">
+              <div>
+                <img src="/Images/relatorio-icone.png" alt="" />
+              </div>
+              <div className="flex items-center text-[#07122D] text-3xl font-medium">
+                <p>Relatórios</p>
+              </div>
+              <div></div>
+              <div>
+                <p>Verificar relatório dos atendimentos </p>
+              </div>
+            </div>
+            <p className="text-[#888585] text-2xl cursor-pointer pt-10">
+              Atendimentos
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
